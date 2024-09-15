53c5856735d8a9cb36a516d206571001228feff8
Joy-sameza <101399643+Joy-sameza@users.noreply.github.com>
2024-09-15 01:15:49 +0100

    add main action functionality

--- README.md
+++ README.md
@@ -1,26 +1,86 @@
-<!-- # auto-merge-action
-This action helps automatically merging pull request when all the checks have passed -->
+# Auto merge action
 
-# Hello world javascript action
+Automatically merge pull request when all the checks have passed
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+## Setup
+
+In order to use this action, you need to give the `write permission` to the repository.
+
+```yaml
+jobs:
+  auto_merge:
+    runs-on: ubuntu-latest
+    permissions:
+      pull-requests: write
+      contents: write
+    steps:
+      # ... Other steps
+
+      - uses: funSamy/auto-merge-action@v1.1.0
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          merge-method: merge
+```
 
 ## Inputs
 
-### `who-to-greet`
+### `github-token`
 
-**Required** The name of the person to greet. Default `"World"`.
+- **Description:** Your GitHub token.
+- **Required:** `true`.
+- **Default:** `null`.
 
-## Outputs
+### `merge-method`
 
-### `time`
-
-The time we greeted you.
+- **Description:** The merge method.
+- **Required:** `false`.
+- **Default:** `squash`.
+- **Options:** `merge`, `squash`, `rebase`.
 
 ## Example usage
 
 ```yaml
-uses: funSamy/auto-merge-action@v1.1
+uses: funSamy/auto-merge-action@v1.1.0
 with:
-  who-to-greet: 'Mona the Octocat'
+  github-token: ${{ secrets.GITHUB_TOKEN }}
 ```
+
+## Getting result from action
+
+```yaml
+# ...
+steps:
+  - name: Try to auto merge PR
+    uses: funSamy/auto-merge-action@v1.1.0
+    id: auto-merge
+    with:
+      github-token: ${{ secrets.GITHUB_TOKEN }}
+
+  - name: Merge results
+    run: |
+      echo sha: ${{ env.sha }}
+      echo merged: ${{ env.merged }}
+      echo message: ${{ env.message }}
+    env:
+      sha: ${{ steps.auto-merge.outputs.sha }}
+      merged: ${{ steps.auto-merge.outputs.merged }}
+      message: ${{ steps.auto-merge.outputs.message }}
+```
+
+## Outputs
+
+### `sha`
+
+- **Description** The SHA of the commit
+
+### `merged`
+
+- **Description** Whether the PR was merged or not
+
+### `message`
+
+- **Description** The message of the mermeg commit
+
+## License
+
+This project is licensed under the [MIT](LICENSE) License.
--- index.js
+++ index.js
@@ -1,15 +1,123 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import {
+  getInput,
+  setFailed,
+  error,
+  notice,
+  warning,
+  setOutput,
+} from "@actions/core";
+import { getOctokit, context as _context } from "@actions/github";
+import { exit } from "node:process";
+
+const githubToken = getInput("github-token", {
+  required: true,
+  trimWhitespace: true,
+});
+
+const mergeMethod = getInput("merge-method");
+
+if (
+  mergeMethod !== "" &&
+  !["merge", "squash", "rebase"].includes(mergeMethod)
+) {
+  setFailed(`Invalid merge method: ${mergeMethod}`);
+  exit(1);
+}
+
+// This GitHub Action will automatically merge pull requests when all the checks have passed
+const octokit = getOctokit(githubToken, {
+  log: {
+    debug: console.log,
+    error: error,
+    info: notice,
+    warn: warning,
+  },
+});
+const context = _context;
+
+// Get the pull request's information
+const prNumber = context.payload.pull_request.number;
+const { owner, repo } = context.repo;
+
+async function run() {
+  const { data } = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: prNumber,
+  });
+  logToActions(data);
+
+  let pr;
+
+  // Poll for the PR's mergeable status
+  for (let i = 0; i < 10; i++) {
+    pr = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: prNumber,
+    });
+
+    if (pr.data.mergeable_state === "clean") {
+      break;
+    } else if (
+      pr.data.mergeable_state === "dirty" ||
+      pr.data.mergeable_state === "unknown"
+    ) {
+      return setFailed("Pull request is not mergeable.");
+    }
+
+    // Wait for a few seconds before retrying
+    logToActions(
+      `Pull request is not mergeable. Retrying in ${5 * (i + 1)} seconds... (${
+        i + 1
+      }/10)`
+    );
+    await new Promise((resolve) => setTimeout(resolve, 5_000 * (i + 1)));
+
+    if (i === 9) {
+      setFailed("Timed out waiting for pull request to become mergeable.");
+      return;
+    }
+  }
+
+  if (pr.data.mergeable_state !== "clean") {
+    setFailed("Timed out waiting for pull request to become mergeable.");
+  }
+
+  const response = await octokit.rest.pulls.merge({
+    owner,
+    repo,
+    // sha: pr.data.merge_commit_sha,
+    pull_number: prNumber,
+    merge_method: mergeMethod || "squash", // Change to 'squash' or 'rebase' if desired
+  });
+  const respData = response.data;
+  logToActions(respData);
+
+  const { sha, merged, message } = respData;
+
+  // Set the output variables
+  setOutput("sha", sha);
+  setOutput("merged", merged);
+  setOutput("message", message);
+}
+
+/**
+ * Logs a message to the GitHub Actions log.
+ * @param {any} message
+ */
+function logToActions(message) {
+  const paramType = typeof message;
+  if (paramType === "object" || paramType === "array") {
+    console.log(JSON.stringify(message, null, 2));
+  } else {
+    console.log(message);
+  }
+  return;
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  await run();
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
